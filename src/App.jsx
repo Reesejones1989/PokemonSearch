3c1c186c2aa8e51006fe62cc470ef297b027490a
Reesejones1989 <mauricejonesjr@gmail.com>
2025-02-04 19:21:14 -0500

    more hashrouterstuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import PokemonSearch from "./PokemonSearch";
 import "./PokemonSearch.css";
